9f1f6361e57f8c877a1927941cffb48c9f1898c7
MentalBlank <mentalblank@live.com>
2017-02-13 10:47:56 +1030

    updated config file

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 /*
  * AlphaFable (DragonFable Private Server) 
  * Made by MentalBlank
- * File: config.php - v0.0.2
+ * File: config.php - v0.0.3
  */
 
 $mysql_host = "localhost";
@@ -10,8 +10,11 @@ $mysql_user = "root";
 $mysql_pass = "root";
 $mysql_name = "alphafable";
 
-$MySQLi = new MySQLi($mysql_host, $mysql_user, $mysql_pass, $mysql_name);
+$MySQLi = new MySQLi($this->mysql_host, $this->mysql_user, $this->mysql_pass, $this->mysql_name);
+if (mysqli_connect_error()) {
+    trigger_error("Failed to connect to MySQL: " . mysql_connect_error(), E_USER_ERROR);
+};
 
 date_default_timezone_set('America/Los_Angeles');
-error_reporting(0);
+error_reporting(1);
 $dateToday = date('Y\-m\-j\TH\:i\:s\.B');
